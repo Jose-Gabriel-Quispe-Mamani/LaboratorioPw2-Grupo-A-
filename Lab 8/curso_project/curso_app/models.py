4887034c3b3799fc1ab674ab316ee3a80c574b88
Jose-Gabriel-Quispe-Mamani <josquispem@unsa.edu.pe>
2025-06-08 17:51:15 -0500

    Relacion de uno a muchos

---
+++ Lab 8/curso_project/curso_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=100)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
